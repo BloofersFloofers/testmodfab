9b1e4b5341563a162f4e66d81db70949a9601fca
BloofersFloofers <braedanham@gmail.com>
2021-07-07 07:38:59 -0700

    Made "normal pills" do something.

--- src/main/java/com/bloofer/stuff/registry/ModItems.java
+++ src/main/java/com/bloofer/stuff/registry/ModItems.java
@@ -1,7 +1,10 @@
 package com.bloofer.stuff.registry;
 
 import com.bloofer.stuff.testmod;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.BlockItem;
+import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
@@ -10,7 +13,13 @@ import net.minecraft.util.registry.Registry;
 public class ModItems {
     //items
 
-    public static final Item NORMAL_PILLS = new Item(new Item.Settings().group(ItemGroup.MISC));
+    public static final Item NORMAL_PILLS = new Item(new Item.Settings()
+            .group(ItemGroup.FOOD)
+            .food(new FoodComponent.Builder().hunger(2).saturationModifier(0.1f).alwaysEdible()
+                    .statusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 60, 0), 0.2f)
+                    .statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 80, 0), 0.2f)
+                    .build())
+    );
 
     //block items
 
